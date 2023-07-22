0f1ee6cc249bbd81cb7fb0bb43be824d0d76811b
Sukanyagantasala <130964308+Sukanyagantasala@users.noreply.github.com>
2023-07-22 16:48:27 +0530

    Create Java Dequeue.java

---
+++ Java Dequeue.java
@@ -0,0 +1,30 @@
+    import java.util.*;
+    public class test {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            Deque deque = new ArrayDeque<>();
+            HashSet<Integer> set = new HashSet<>();
+
+           int n = in.nextInt();
+           int m = in.nextInt();
+           int max = Integer.MIN_VALUE;
+
+           for (int i = 0; i < n; i++) {
+            int k = in.nextInt();
+
+            deque.add(k);
+            set.add(k);
+
+            if (deque.size() == m) {
+                if (set.size() > max)
+                    max = set.size();
+
+                int first = (int) deque.remove();
+                if (!deque.contains(first))
+                    set.remove(first);
+            }
+        }
+        System.out.println(max);
+    }
+}
+
